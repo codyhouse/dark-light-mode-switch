1b30df958aaf94d18b5bea5aa9016291f11e6f9c
claudia-romano <claudia.romano06@gmail.com>
2019-04-03 09:24:14 +0100

    Save theme selection using Local Storage API
    Co-Authored-By: Sebastiano Guerriero <sebastiano-guerriero@users.noreply.github.com>

--- main/assets/js/main.js
+++ main/assets/js/main.js
@@ -2,14 +2,28 @@
 	// Theme switch
 	var themeSwitch = document.getElementById('themeSwitch');
 	if(themeSwitch) {
-		resetTheme(); // if checked -> add the data-theme attribute to the body
+		initTheme(); // if user has already selected a specific theme -> apply it
 		themeSwitch.addEventListener('change', function(event){
-    	resetTheme();
+    	resetTheme(); // update color theme
     });
 
+    function initTheme() {
+    	var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark');
+    	// update checkbox
+    	themeSwitch.checked = darkThemeSelected;
+			// update body data-theme attribute
+			darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+    };
+
     function resetTheme() {
-    	(themeSwitch.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
-    }
+    	if(themeSwitch.checked) { // dark theme has been selected
+    		document.body.setAttribute('data-theme', 'dark');
+    		localStorage.setItem('themeSwitch', 'dark');
+    	} else {
+    		document.body.removeAttribute('data-theme');
+    		localStorage.removeItem('themeSwitch');
+    	} 
+    };
 	}
 	
 
